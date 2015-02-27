82ce6ec0c0d6eda7a2b1ca1a6aa8b617eb0ce547
Miguel Young <myoungg93@uwo.ca>
2015-02-27 14:06:22 -0500

    upload weatherviewer 0.0.1

---
+++ weatherviewer/src/main/java/group17/weatherviewer/Background.java
@@ -0,0 +1,28 @@
+package group17.weatherviewer;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Background extends JPanel {
+	private Image image;
+
+	public Background(String image) {
+		this(new ImageIcon(image).getImage());
+	}
+
+	public Background(Image image) {
+		this.image = image;
+		Dimension size = new Dimension(image.getWidth(null),
+				image.getHeight(null));
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		setSize(size);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(image, 0, 0, null);
+	}
+}
