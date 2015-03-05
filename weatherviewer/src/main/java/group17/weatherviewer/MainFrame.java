3c3c78d47ddfb051cd961b9544218f3ca6034412
Tim Etchells <tetchel@gmail.com>
2015-03-04 22:07:26 -0500

    Added UserPreferences, changed LocationException to WeatherException, Fixed absolute icon paths

--- weatherviewer/src/main/java/group17/weatherviewer/MainFrame.java
+++ weatherviewer/src/main/java/group17/weatherviewer/MainFrame.java
@@ -454,6 +454,7 @@ public class MainFrame {
             System.out.println(prefs.getTempUnits());
             UserPreferences.savePrefs(prefs);
 
+            //create a new preferences and load the old one
             UserPreferences new_prefs = UserPreferences.getPrefs();
             //still f instead of default c because we saved & loaded it
             System.out.println(new_prefs.getTempUnits());
