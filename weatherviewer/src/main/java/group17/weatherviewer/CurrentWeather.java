76aac69027bb910a850129f97774c3b1b50f752e
Nicole Kovacs <nkovac2@uwo.ca>
2015-03-26 15:21:55 -0400

    Wind direction converted to string

--- weatherviewer/src/main/java/group17/weatherviewer/CurrentWeather.java
+++ weatherviewer/src/main/java/group17/weatherviewer/CurrentWeather.java
@@ -149,7 +149,27 @@ public class CurrentWeather {
      * @return the current wind direction in degrees
      */
 	public String getWindDirection() {
-		return String.valueOf(windDir);
+		System.out.println(windDir);
+		if(windDir > 337.5 && windDir <= 360)
+			return "N";
+		else if(windDir >= 0 && windDir <= 22.5)
+			return "N";
+		else if(windDir > 22.5 && windDir <= 67.5)
+			return "NE";
+		else if(windDir > 67.5 && windDir <= 112.5)
+			return "E";
+		else if(windDir > 112.5 && windDir <= 157.5)
+			return "SE";
+		else if(windDir > 157.5 && windDir <= 202.5)
+			return "S";
+		else if(windDir > 202.5 && windDir <= 247.5)
+			return "SW";
+		else if(windDir > 247.5 && windDir <= 292.5)
+			return "W";
+		else if(windDir > 292.5 && windDir <= 337.5)
+			return "NW";
+		else
+			return "?";
 	}
 
     /**
